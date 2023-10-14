f1c9f289a4fd6350c06b04c2f2abd40dc95f6269
Felicio Almeida <feliciorar@gmail.com>
2023-10-14 19:44:15 -0300

    error treatment

--- src/main/java/br/com/felicioalmeida/todolist/task/TaskController.java
+++ src/main/java/br/com/felicioalmeida/todolist/task/TaskController.java
@@ -54,12 +54,25 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-        
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var task = this.taskRepository.findById(id).orElse(null);
+        
+        if (task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuário não tem permissão par alterar essa tarefa");
+        }
 
         Utils.copyNoNullProperties(taskModel, task);
+        
+        var taskUpdated = this.taskRepository.save(task);
 
-        return this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
